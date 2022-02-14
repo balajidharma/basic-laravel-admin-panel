bbb96cda36c19ee6b580828d48c38d9db51fccea
Balaji D <balajidharma@2610158.no-reply.drupal.org>
2022-02-13 22:45:42 -0500

    Added permission CRUD

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('permission.index')" :active="request()->routeIs('permission.index')">
+                        {{ __('Permission') }}
+                    </x-nav-link>
                 </div>
             </div>
 
---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::group([
+    'namespace'  => 'App\Http\Controllers\Admin',
+    'prefix'     => 'admin',
+    'middleware' => ['auth'],
+], function () {
+    Route::resource('permission', 'PermissionController');
+});
