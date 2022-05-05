1d84cb1edb3ab719c0774abcdfc9bd895836868f
balajidharma <balajidharma@hotmail.com>
2022-05-04 22:39:56 -0400

    Users UI inital commit

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,7 @@ Route::group([
     'prefix'     => 'admin',
     'middleware' => ['auth'],
 ], function () {
+    Route::resource('user', 'UserController');
     Route::resource('role', 'RoleController');
     Route::resource('permission', 'PermissionController');
 });
