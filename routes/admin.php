465bfedd77e55742f5f4a400d31898e855c502de
Balaji D <balajidharma@2610158.no-reply.drupal.org>
2022-05-04 21:51:39 -0400

    Added account info edit page

--- routes/admin.php
+++ routes/admin.php
@@ -8,4 +8,7 @@ Route::group([
     Route::resource('user', 'UserController');
     Route::resource('role', 'RoleController');
     Route::resource('permission', 'PermissionController');
+    Route::get('edit-account-info', 'UserController@accountInfo')->name('admin.account.info');
+    Route::post('edit-account-info', 'UserController@accountInfoStore')->name('admin.account.info.store');
+    Route::post('change-password', 'UserController@changePasswordStore')->name('admin.account.password.store');
 });
