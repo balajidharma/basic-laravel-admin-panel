5423a8e1ec0162e51a97045705c7a1ebd7f181ab
balajidharma <balajidharma@hotmail.com>
2022-07-03 10:17:48 -0400

    Migrating from Laravel Mix to Vite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    plugins: [
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
+    ],
+});
