73d095173ee1601e2641fc360bfb61095c1e49c4
balajidharma <balajidharma@hotmail.com>
2022-07-03 11:23:50 -0400

    Updated view with Reusable Blade Components

--- resources/views/admin/role/edit.blade.php
+++ resources/views/admin/role/edit.blade.php
@@ -1,66 +1,49 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+<x-admin.wrapper>
+    <x-slot name="title">
             {{ __('Roles') }}
-        </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="px-6">
-                    <h1 class="inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 py-4 block sm:inline-block flex">{{ __('Update role') }}</h1>
-                    <a href="{{route('role.index')}}" class="no-underline hover:underline text-cyan-600 dark:text-cyan-400">{{ __('<< Back to all role') }}</a>
-                    @if ($errors->any())
-                        <ul class="mt-3 list-none list-inside text-sm text-red-400">
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                </div>
-                <div class="w-full px-6 py-4 bg-white overflow-hidden">
+    <div>
+        <x-admin.breadcrumb href="{{route('role.index')}}" title="{{ __('Update role') }}">{{ __('<< Back to all roles') }}</x-admin.breadcrumb>
+        <x-admin.form.errors />
+    </div>
+    <div class="w-full py-2 bg-white overflow-hidden">
 
-                    <form method="POST" action="{{ route('role.update', $role->id) }}">
-                    @csrf
-                    @method('PUT')
+        <form method="POST" action="{{ route('role.update', $role->id) }}">
+        @csrf
+        @method('PUT')
 
-                        <div class="py-2">
-                            <label for="name" class="block font-medium text-sm text-gray-700{{$errors->has('name') ? ' text-red-400' : ''}}">{{ __('Name') }}</label>
+            <div class="py-2">
+            <x-admin.form.label for="name" class="{{$errors->has('name') ? 'text-red-400' : ''}}">{{ __('Name') }}</x-admin.form.label>
 
-                            <input id="name" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full{{$errors->has('name') ? ' border-red-400' : ''}}"
-                                            type="text"
-                                            name="name"
-                                            value="{{ old('name', $role->name) }}"
-                                            />
+            <x-admin.form.input id="name" class="{{$errors->has('name') ? 'border-red-400' : ''}}"
+                                type="text"
+                                name="name"
+                                value="{{ old('name', $role->name) }}"
+                                />
+            </div>
+
+            @unless ($role->name == env('APP_SUPER_ADMIN', 'super-admin'))
+            <div class="py-2">
+                <h3 class="inline-block text-xl sm:text-2xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 py-4 block sm:inline-block flex">Permissions</h3>
+                <div class="grid grid-cols-4 gap-4">
+                    @forelse ($permissions as $permission)
+                        <div class="col-span-4 sm:col-span-2 md:col-span-1">
+                            <label class="form-check-label">
+                                <input type="checkbox" name="permissions[]" value="{{ $permission->name }}" {{ in_array($permission->id, $roleHasPermissions) ? 'checked' : '' }} class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                {{ $permission->name }}
+                            </label>
                         </div>
-
-                        @unless ($role->name == env('APP_SUPER_ADMIN', 'super-admin'))
-                        <div class="py-2">
-                            <h3 class="inline-block text-xl sm:text-2xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 py-4 block sm:inline-block flex">Permissions</h3>
-                            <div class="grid grid-cols-4 gap-4">
-                                @forelse ($permissions as $permission)
-                                    <div class="col-span-4 sm:col-span-2 md:col-span-1">
-                                        <label class="form-check-label">
-                                            <input type="checkbox" name="permissions[]" value="{{ $permission->name }}" {{ in_array($permission->id, $roleHasPermissions) ? 'checked' : '' }} class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-                                            {{ $permission->name }}
-                                        </label>
-                                    </div>
-                                @empty
-                                    ----
-                                @endforelse
-                            </div>
-                        </div>
-                        @endunless
-
-                        <div class="flex justify-end mt-4">
-                            <button type='submit' class='inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150'>
-                                {{ __('Update') }}
-                            </button>
-                        </div>
-                    </form>
+                    @empty
+                        ----
+                    @endforelse
                 </div>
             </div>
-        </div>
+            @endunless
+
+            <div class="flex justify-end mt-4">
+                <x-admin.form.button>{{ __('Update') }}</x-admin.form.button>
+            </div>
+        </form>
     </div>
-</x-app-layout>
+</x-admin.wrapper>
--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -1,92 +1,67 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Roles') }}
-        </h2>
+<x-admin.wrapper>
+    <x-slot name="title">
+        {{ __('Roles') }}
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="flex flex-col mt-8">
-                    @can('role create')
-                    <div class="d-print-none with-border mb-8">
-                        <a href="{{ route('role.create') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ __('Add Role') }}</a>
-                    </div>
-                    @endcan
-                        <div class="py-2">
-                        @if(session()->has('message'))
-                            <div class="mb-8 text-green-400 font-bold">
-                                {{ session()->get('message') }}
+    @can('role create')
+    <x-admin.add-link href="{{ route('role.create') }}">
+        {{ __('Add Role') }}
+    </x-admin.add-link>
+    @endcan
+
+    <div class="py-2">
+        <x-admin.message />
+        <div class="min-w-full border-b border-gray-200 shadow overflow-x-auto">
+            <x-admin.grid.search action="{{ route('role.index') }}" />
+            <x-admin.grid.table>
+                <x-slot name="head">
+                    <tr>
+                        <x-admin.grid.th>
+                            @include('admin.includes.sort-link', ['label' => 'Name', 'attribute' => 'name'])
+                        </x-admin.grid.th>
+                        @canany(['role edit', 'role delete'])
+                        <x-admin.grid.th>
+                            {{ __('Actions') }}
+                        </x-admin.grid.th>
+                        @endcanany
+                    </tr>
+                </x-slot>
+                <x-slot name="body">
+                @foreach($roles as $role)
+                    <tr>
+                        <x-admin.grid.td>
+                            <div class="text-sm text-gray-900">
+                                <a href="{{route('role.show', $role->id)}}" class="no-underline hover:underline text-cyan-600 dark:text-cyan-400">{{ $role->name }}</a>
                             </div>
-                        @endif
-                            <div class="min-w-full border-b border-gray-200 shadow overflow-x-auto">
-                                <form method="GET" action="{{ route('role.index') }}">
-                                <div class="py-2 flex">
-                                    <div class="flex pl-2">
-                                        <input type="search" name="search" value="{{ request()->input('search') }}" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Search">
-                                        <button type='submit' class='ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150'>
-                                            {{ __('Search') }}
-                                        </button>
-                                    </div>
+                        </x-admin.grid.td>
+                        @canany(['role edit', 'role delete'])
+                        <x-admin.grid.td>
+                            <form action="{{ route('role.destroy', $role->id) }}" method="POST">
+                                <div class="flex">
+                                    @can('role edit')
+                                    <a href="{{route('role.edit', $role->id)}}" class="px-4 py-2 text-white mr-4 bg-blue-600">
+                                        {{ __('Edit') }}
+                                    </a>
+                                    @endcan
+
+                                    @can('role delete')
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="px-4 py-2 text-white bg-red-600" onclick="return confirm('{{ __('Are you sure you want to delete?') }}')">
+                                        {{ __('Delete') }}
+                                    </button>
+                                    @endcan
                                 </div>
-                                </form>
-                                <table class="border-collapse table-auto w-full text-sm">
-                                    <thead>
-                                        <tr>
-                                            <th class="py-4 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-left">
-                                                @include('admin.includes.sort-link', ['label' => 'Name', 'attribute' => 'name'])
-                                            </th>
-                                            @canany(['role edit', 'role delete'])
-                                            <th class="py-4 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-left">
-                                                {{ __('Actions') }}
-                                            </th>
-                                            @endcanany
-                                        </tr>
-                                    </thead>
-
-                                    <tbody class="bg-white dark:bg-slate-800">
-                                        @foreach($roles as $role)
-                                        <tr>
-                                            <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
-                                                <div class="text-sm text-gray-900">
-                                                    <a href="{{route('role.show', $role->id)}}" class="no-underline hover:underline text-cyan-600 dark:text-cyan-400">{{ $role->name }}</a>
-                                                </div>
-                                            </td>
-                                            @canany(['role edit', 'role delete'])
-                                            <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
-                                                <form action="{{ route('role.destroy', $role->id) }}" method="POST">
-                                                    <div class="flex">
-                                                        @can('role edit')
-                                                        <a href="{{route('role.edit', $role->id)}}" class="px-4 py-2 text-white mr-4 bg-blue-600">
-                                                            {{ __('Edit') }}
-                                                        </a>
-                                                        @endcan
-
-                                                        @can('role delete')
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button class="px-4 py-2 text-white bg-red-600" onclick="return confirm('{{ __('Are you sure you want to delete?') }}')">
-                                                            {{ __('Delete') }}
-                                                        </button>
-                                                        @endcan
-                                                    </div>
-                                                </form>
-                                            </td>
-                                            @endcanany
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                            <div class="py-8">
-                                {{ $roles->appends(request()->query())->links() }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                            </form>
+                        </x-admin.grid.td>
+                        @endcanany
+                    </tr>
+                    @endforeach
+                </x-slot>
+            </x-admin.grid.table>
+        </div>
+        <div class="py-8">
+            {{ $roles->appends(request()->query())->links() }}
         </div>
     </div>
-</x-app-layout>
+</x-admin.wrapper>
