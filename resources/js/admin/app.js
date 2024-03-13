44e82b6f8ebf2244e7ba763f13a02c13b28e7312
balajidharma <balajidharma@hotmail.com>
2024-03-13 18:39:43 -0400

    updated package.json

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -1,11 +1,7 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
 import { themeChange } from 'theme-change';
 
-window.Alpine = Alpine;
-
-Alpine.start();
 themeChange();
 
 var selectedTheme = localStorage.getItem("theme");
