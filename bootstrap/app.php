c155fec121951846f07e3044868210bca7d92753
balajidharma <balajidharma@hotmail.com>
2024-03-29 10:48:08 -0400

    Create bootstrap/providers.php and moved providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        using: function () {     
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-
+        then: function () {
             Route::middleware('web')
                 ->group(base_path('routes/admin.php'));
         },
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\EventServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
     App\Providers\ViewServiceProvider::class,
 ];
